8ae0545bc65dd38a3d8ab935198d03cd2945d012
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2021-03-29 21:52:45 +0100

    Worker now opens subchannel for API communication.

--- worker.js
+++ worker.js
@@ -3,8 +3,8 @@ importScripts("ipc.js")
 addEventListener('message', ev => console.log('Worker incoming', ev.data))
 async function main() 
 {
-    //const port = await getSubChannel(this);
-    const client = await getClient(this);
+    const port = await getSubChannel(this);
+    const client = await getClient(port);
     console.log('Worker got client')
     await client.show(["http://example.com"]);
     console.log("Yeee-");
